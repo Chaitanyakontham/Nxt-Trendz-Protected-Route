8fb983c372f3ee8665d16966deaf3b32ae09fd67
Chaitanyakontham <167058078+Chaitanyakontham@users.noreply.github.com>
2024-05-20 20:56:40 +0530

    Update index.js

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -23,7 +23,7 @@ class LoginForm extends Component {
   onSubmitSuccess = jwtToken => {
     const {history} = this.props
 
-    Cookies.set('jwt-token', jwtToken, {
+    Cookies.set('jwt_token', jwtToken, {
       expires: 30,
     })
     history.replace('/')
