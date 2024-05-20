2c57a176ee5a42508d90c036c80536b3e8154bbe
Chaitanyakontham <167058078+Chaitanyakontham@users.noreply.github.com>
2024-05-20 23:35:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import './App.css'
 const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/products" component={Products} />
-    <Route exact path="/cart" component={Cart} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
     <Route component={NotFound} />
   </Switch>
 )
